affe53b2fe2ee6f6545d12b1ad32e9f844a33403
Chetan Malhotra <chetanalwaysmalhotra@hotmail.com>
2019-05-22 02:27:57 +0530

    commit

---
+++ install-libkloudtrader.sh
@@ -0,0 +1,9 @@
+apt-get install -y wget gcc build-essential python3-dev python3-setuptools
+export PATH=/usr/local/bin:$PATH
+git clone https://github.com/debuggermalhotra/install_libkloudtrader.git
+cd install_libkloudtrader
+./configure --prefix=/usr
+make
+make install
+pip3 install pipenv
+pipenv install --system --deploy
